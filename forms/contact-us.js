18026223c89e25b99bc34482fcb2afd29f7283e2
Jonathan Yeo <jonathanyji98_28@hotmail.com>
2023-02-12 00:15:25 +1000

    test again

--- forms/contact-us.js
+++ forms/contact-us.js
@@ -1,11 +1,11 @@
 function sendEmail() {
-    console.log("SEND EMAIL BUTTON 3")
+    console.log("SEND EMAIL BUTTON 4")
     Email.send({
         SecureToken : "18GPG1N7N6RR3bMiaDEneMzWPXKFBECqxq",
-        To : 'jonathanyji98@hotmail.com',
+        To : "jonathanyji98@hotmail.com",
         From : "iamjonny98@gmail.com",
         Subject : "Test email",
-        Body : "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
+        Body : "This is just a test email"
     }).then(
       message => alert(message)
     );
