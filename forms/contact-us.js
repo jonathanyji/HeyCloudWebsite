65b0e117884402201ed32971693d9293cf359758
Jonathan Yeo <jonathanyji98_28@hotmail.com>
2023-02-11 23:53:54 +1000

    test

--- forms/contact-us.js
+++ forms/contact-us.js
@@ -2,10 +2,10 @@ function sendEmail() {
     console.log("SEND EMAIL BUTTON")
     Email.send({
         Host : "smtp.elasticemail.com",
-        Username : "iamjonny98@gmail.com",
-        Password : "18GPG1N7N6RR3bMiaDEneMzWPXKFBECqxq",
+        Username : "jonathanyji98@hotmail.com",
+        Password : "732819FFAB592D96FE2D7B3103A91CA4B6FE",
         To : 'iamjonny98@gmail.com',
-        From : "iamjonny98@gmail.com",
+        From : "jonathanyji98@hotmail.com",
         Subject : "Test email",
         Body : "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
     }).then(
