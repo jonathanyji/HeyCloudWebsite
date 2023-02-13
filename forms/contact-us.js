dd42af134c43d995d9c4702b556274cf50490b63
Jonathan Yeo <jonathanyji98_28@hotmail.com>
2023-02-13 11:32:54 +1000

    test

--- forms/contact-us.js
+++ forms/contact-us.js
@@ -3,4 +3,5 @@ function sendEmailFrom(){
     var subject = document.getElementById("subject").value;
     var body = document.getElementById("message").value;
     window.location.href = "mailto:" + email + "?subject=" + subject + "&body=" + body;
+    return false;
 }
