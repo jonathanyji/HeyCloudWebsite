a034a1a5de2ee4e33c7ea47add8ecf7a952a0e0c
Jonathan Yeo <jonathanyji98_28@hotmail.com>
2023-02-13 11:00:43 +1000

    changed contact us map and clean up

--- forms/contact-us.js
+++ forms/contact-us.js
@@ -1,5 +1,4 @@
 function sendEmailFrom(){
-    console.log("TEST SIMPLE SEND EMAIL BUTTON")
     var email = "admin@heycloud.com.sg";
     var subject = document.getElementById("subject").value;
     var body = document.getElementById("message").value;
