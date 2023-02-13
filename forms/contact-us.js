9cd369e2c8be9d3fa2968e0b56bce8eef7ed2eec
Jonathan Yeo <jonathanyji98_28@hotmail.com>
2023-02-13 10:37:19 +1000

    test test test

--- forms/contact-us.js
+++ forms/contact-us.js
@@ -1,12 +1,7 @@
-function sendEmail() {
-    console.log("SEND EMAIL BUTTON 4")
-    Email.send({
-        SecureToken : "18GPG1N7N6RR3bMiaDEneMzWPXKFBECqxq",
-        To : "jonathanyji98@hotmail.com",
-        From : "iamjonny98@gmail.com",
-        Subject : "Test email",
-        Body : "This is just a test email"
-    }).then(
-      message => alert(message)
-    );
+function sendEmailFrom(){
+    console.log("TEST SIMPLE SEND EMAIL BUTTON")
+    var email = "admin@heycloud.com.sg";
+    var subject = document.getElementById("subject").value;
+    var body = document.getElementById("message").value;
+    window.location.href = "mailto:" + email + "?subject=" + subject + "&body=" + body;
 }
